0c6ff0a2e2dfceea688cc826a5e89e6d721957e4
syaoyaoyao <syao0@chicagobooth.edu>
2023-01-24 04:25:51 +0000

    assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,49 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "We tie!"
+    elsif @comp_move == "paper"
+      @outcome = "We lost!"
+    else
+      @outcome = "We won!"
+    end
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "paper"
+      @outcome = "We tie!"
+    elsif @comp_move == "scissors"
+      @outcome = "We lost!"
+    else
+      @outcome = "We won!"
+    end
+
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "scissors"
+      @outcome = "We tie!"
+    elsif @comp_move == "rock"
+      @outcome = "We lost!"
+    else
+      @outcome = "We won!"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb" })
+  end
+
+  def home_page
+    render({ :template => "game_templates/home_page.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-
-
+  get("/", { :controller => "application", :action => "home_page" })
+  get("/rock", { :controller => "application", :action => "play_rock" })
+  get("/scissors", { :controller => "application", :action => "play_scissors" })
+  get("/paper", { :controller => "application", :action => "play_paper" })
 end
